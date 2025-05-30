24d8764fe64709861559bb321850b38ccacc3510
anandhu <anandhukavumkal@gmail.com>
2025-05-30 13:15:53 +0530

    FIX: some play and playbook storage errors fixed

--- src/components/molecules/BackgroundLayout.tsx
+++ src/components/molecules/BackgroundLayout.tsx
@@ -7,9 +7,9 @@ export default function BackgroundLayout({ children, isHighlighter = false }: { 
         <div className={`relative h-screen flex justify-center items-center`}>
 
             <div className={`absolute inset-0`}>
-                <div className={`absolute inset-0 -z-10 h-full w-full bg-darkPrimary bg-[linear-gradient(to_right,#E1EACD10_1px,transparent_1px),linear-gradient(to_bottom,#E1EACD10_1px,transparent_1px)] bg-[size:2rem_6rem]`}></div>
+                <div className={`absolute inset-0 -z-10 h-full w-full bg-darkPrimary bg-[linear-gradient(to_right,#74747410_1px,transparent_1px),linear-gradient(to_bottom,#74747410_1px,transparent_1px)] bg-[size:2rem_2rem]`}></div>
             </div>
-            <div className="absolute inset-0 -z-10 flex w-2/3 h-full flex-col items-center justify-center px-4 bg-[radial-gradient(circle_at_center,black,transparent)]">
+            <div className="absolute inset-0 left-[18%] -z-10 flex w-2/3 h-full flex-col items-center justify-center px-4 bg-[radial-gradient(circle_at_center,black,transparent)]">
                 <div className={`absolute inset-0`}>
                     <div className={`absolute inset-0 -z-20 h-full w-full ${isHighlighter ? 'bg-[size:14px_24px] [&>div]:absolute [&>div]:left-0 [&>div]:right-0 [&>div]:top-0 [&>div]:-z-20 [&>div]:m-auto [&>div]:h-[400px] [&>div]:w-[400px] [&>div]:rounded-full [&>div]:bg-[#C95792] [&>div]:opacity-60 [&>div]:blur-[100px] [&>div]:bg-[size:14px_24px]' : ''}`}>
                         <div></div>

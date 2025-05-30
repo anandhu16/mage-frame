8043961819c80580928978d5d7fc0aeb8f457663
Anandhu Unnikrishnan <85945588+anandhu16@users.noreply.github.com>
2025-05-30 12:30:21 +0530

    Fix: build error fix

--- src/utils/helpers/classMerge.ts
+++ src/utils/helpers/classMerge.ts
@@ -1,13 +1,11 @@
-
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-
 
 /**
  * Merge class names with Tailwind CSS classes
  * @param {string[]} inputs - Class names to merge
  * @returns {string} - Merged class names
  */
-export function cn(...inputs: string[]): string {
-    return twMerge(clsx(inputs));
+export function cn(...inputs: (string | undefined)[]): string {
+  return twMerge(clsx(inputs));
 }
